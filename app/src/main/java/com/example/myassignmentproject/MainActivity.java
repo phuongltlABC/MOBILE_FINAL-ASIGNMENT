c0d505a3e510e304a073d44c7bb7f193b7bac16a
phuongltlABC <157083799+phuongltlABC@users.noreply.github.com>
2025-07-06 19:47:52 +0700

    Initial commit

---
+++ app/src/main/java/com/example/myassignmentproject/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.myassignmentproject;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.example.myassignmentproject.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        ActivityMainBinding binding;
+        super.onCreate(savedInstanceState);
+
+    }
+}
