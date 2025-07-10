77d5e3e4163375b25757e726fd9eda4b321df5ea
ngnnh <tdngananh218@gmail.com>
2025-07-10 11:21:49 +0700

    change app icon + create Splash screen and Onboarding

--- app/src/main/java/com/example/myassignmentproject/MainActivity.java
+++ app/src/main/java/com/example/myassignmentproject/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.myassignmentproject;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.LayoutInflater;
 
 import androidx.activity.EdgeToEdge;
@@ -12,11 +14,22 @@ import androidx.core.view.WindowInsetsCompat;
 import com.example.myassignmentproject.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+    public static int SPLASH_TIMER = 800;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         ActivityMainBinding binding;
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(MainActivity.this, Onboarding.class);
+                startActivity(intent);
+                finish();
+            }
+        }, SPLASH_TIMER);
 
     }
 }
